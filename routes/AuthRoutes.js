a1b56ff1f2524fd7845df4dec021ea977287b6b2
Abraham <abrahamnhier97@gmail.com>
2024-07-04 22:20:27 +0300

    typo

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, logout } = require('../controllers/authController');
+const { register, login, logout } = require('../controllers/AuthController');
 
 
 router.post('/register', register);
